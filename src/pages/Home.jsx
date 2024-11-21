9984cf9f195b8a575c7707937a69b266047b19a8
04stefke <jovanovics399@yahoo.com>
2024-11-21 17:28:03 +0000

    imported necessary files into home and app

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import Products from "../components/Products";
+import Cart from "../components/Cart";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+	return (
+		<div>
+			<h1>Desserts</h1>
+			<Products />
+			<Cart />
+		</div>
+	);
+};
 
-export default Home
+export default Home;
